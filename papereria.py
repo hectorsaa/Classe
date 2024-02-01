ac0099bb441f5573b0f34276339c1b7a8c0202f4
Hèctor Saa <h.saa07@insfredericmompou.cat>
2024-02-01 14:24:17 +0100

    volum ventas

--- papereria.py
+++ papereria.py
@@ -2,7 +2,12 @@ import time
 
 clients_compra=['paper corporation','WestRock','UPM']
 clients_venta=['corte ingles','La Sol papereria','Amazon']
-zonas=['A','B','C']
+volum_vendas=[]
+paper_venutA2=[]
+paper_venutA3=[]
+paper_venutA4=[]
+paper_venutA5=[]
+paper_venutA6=[]
 
 usuari=str(input("Introdueix nom d'usuari: "))
 codi=int(input("Introdueix el teu codi: "))
@@ -26,10 +31,12 @@ if codi== 101 and usuari=="Stanley Hudson" or codi== 102 and usuari=="Angela Mar
                         4-Tancar programa
                         """))
         if opcio==1:
+            time.sleep(3)
             print(f"La empresa té {diners}€")
-            break
+            
         
         elif opcio==2:
+            time.sleep(3)          
             if diners<=100:
                 print("No hi han suficients diners a la empresa")
                 break
@@ -39,6 +46,7 @@ if codi== 101 and usuari=="Stanley Hudson" or codi== 102 and usuari=="Angela Mar
                     opcio_papers=str(input("Quin tipues de paper vols comprar (A2, A3, A4, A5, A6) x-tornar a la pàgina anterior: " ))
                 
                     if opcio_papers=="A2":
+                        time.sleep(3)
                         print("Un paquet de A2 de 500ut costa 18,95€")
                         unitats_paper=int(input("Quantitat de paquets que vols comprar: "))
                         descompte=unitats_paper//4
@@ -57,6 +65,7 @@ if codi== 101 and usuari=="Stanley Hudson" or codi== 102 and usuari=="Angela Mar
                             break
 
                     elif opcio_papers=="A3":
+                        time.sleep(3)
                         print("Un paquet de A3 de 500ut costa 11,34€")
                         unitats_paper=int(input("Quantitat de paquets que vols comprar: "))
                         preu=7.98
@@ -79,6 +88,7 @@ if codi== 101 and usuari=="Stanley Hudson" or codi== 102 and usuari=="Angela Mar
                             break
                     
                     elif opcio_papers=="A4":
+                        time.sleep(3)
                         print("Un paquet de A4 de 500ut costa 7,98€")
                         unitats_paper=int(input("Quantitat de paquets que vols comprar: "))
                         preu=11.34
@@ -100,6 +110,7 @@ if codi== 101 and usuari=="Stanley Hudson" or codi== 102 and usuari=="Angela Mar
                             break
                 
                     elif opcio_papers=="A5":
+                        time.sleep(3)
                         print("Un paquet de A5 de 500ut costa 7,13€")
                         unitats_paper=int(input("Quantitat de paquets que vols comprar: "))
                         descompte=unitats_paper//4
@@ -117,6 +128,7 @@ if codi== 101 and usuari=="Stanley Hudson" or codi== 102 and usuari=="Angela Mar
                             print("Compra cancel·lada")
                             break
                     elif opcio_papers=="A6":
+                        time.sleep(3)
                         print("Un paquet de A6 de 500ut costa 6,45€")
                         unitats_paper=int(input("Quantitat de paquets que vols comprar: "))
                         descompte=unitats_paper//4
@@ -138,7 +150,8 @@ if codi== 101 and usuari=="Stanley Hudson" or codi== 102 and usuari=="Angela Mar
                         time.sleep(3)
                         break
         
-        elif opcio ==3:
+        elif opcio ==3:           
+            time.sleep(3)
             while True:
                 opcio_clients=int(input("""Que vols fer:
                                         1-veure clients
@@ -146,7 +159,8 @@ if codi== 101 and usuari=="Stanley Hudson" or codi== 102 and usuari=="Angela Mar
                                         3-eliminar clients 
                                         4-tornar 
                                         """))
-                if opcio_clients==1:
+                time.sleep(3)                
+                if opcio_clients==1:                    
                     print(f"Els clients són{clients_compra}" )
                 
                 if opcio_clients==2:
@@ -164,6 +178,7 @@ if codi== 101 and usuari=="Stanley Hudson" or codi== 102 and usuari=="Angela Mar
                     break
             
         elif opcio==4:
+            time.sleep(3)
             print("Tancant programa ...")
             time.sleep(4)
             break
@@ -183,12 +198,16 @@ elif codi== 103 and usuari=="Dwight Schrute" or codi==104 and usuari=="Jim Halpe
                         6-Tancar programa
                         """))
         if opcio==1:
-            print(f"La empresa té {diners}€")
-            break
+            print(f"La empresa té al magatzem {A2} paquets de A2")
+            print(f"La empresa té al magatzem {A3} paquets de A3")
+            print(f"La empresa té al magatzem {A4} paquets de A4")
+            print(f"La empresa té al magatzem {A5} paquets de A5")
+            print(f"La empresa té al magatzem {A6} paquets de A6")            
         
         elif opcio==2:
             while True:
                 opcio_papers=str(input("Quin tipues de papeers vols vendre (A2, A3, A4, A5, A6)   x-Tornar a la pàgina anterior " ))
+                time.sleep(3)
                 
                 if opcio_papers=="A2":
                     if A2<=10:
@@ -205,6 +224,8 @@ elif codi== 103 and usuari=="Dwight Schrute" or codi==104 and usuari=="Jim Halpe
                             print("Venda realitzada amb èxit")
                             diners=diners+preu_total
                             A2=A2-quantitat_vendre
+                            volum_vendas.append(preu_total)
+                            paper_venutA2.append(quantitat_vendre)
                             break
                         elif confirmar_venda=="no":
                             print("Venda cancel·lada")
@@ -228,6 +249,8 @@ elif codi== 103 and usuari=="Dwight Schrute" or codi==104 and usuari=="Jim Halpe
                                 print("Venda realitzada amb èxit")
                                 diners=diners+preu_total
                                 A3=A3-quantitat_vendre
+                                volum_vendas.append(preu_total)
+                                paper_venutA3.append(quantitat_vendre)
                                 break
                             elif confirmar_venda=="no":
                                 print("Venda cancel·lada")
@@ -251,6 +274,8 @@ elif codi== 103 and usuari=="Dwight Schrute" or codi==104 and usuari=="Jim Halpe
                                 print("Venda realitzada amb èxit")
                                 diners=diners+preu_total
                                 A3=A3-quantitat_vendre
+                                volum_vendas.append(preu_total)
+                                paper_venutA4.append(quantitat_vendre)
                                 break
                             elif confirmar_venda=="no":
                                 print("Venda cancel·lada")
@@ -270,6 +295,8 @@ elif codi== 103 and usuari=="Dwight Schrute" or codi==104 and usuari=="Jim Halpe
                             print("Venda realitzada amb èxit")
                             diners=diners+preu_total
                             A5=A5-quantitat_vendre
+                            volum_vendas.append(preu_total)
+                            paper_venutA5.append(quantitat_vendre)
                             break
                         elif confirmar_venda=="no":
                             print("Venda cancel·lada")
@@ -289,6 +316,8 @@ elif codi== 103 and usuari=="Dwight Schrute" or codi==104 and usuari=="Jim Halpe
                             print("Venda realitzada amb èxit")
                             diners=diners+preu_total
                             A6=A6-quantitat_vendre
+                            volum_vendas.append(preu_total)
+                            paper_venutA6.append(quantitat_vendre)
                             break
                         elif confirmar_venda=="no":
                             print("Venda cancel·lada")
@@ -300,6 +329,7 @@ elif codi== 103 and usuari=="Dwight Schrute" or codi==104 and usuari=="Jim Halpe
                                         2-Afegir clients
                                         3-Eliminar clients 
                                         4-Tornar """))
+                time.sleep(3)
                 if opcio_clients==1:
                     print(f"Els clients són{clients_venta}" )
                 
@@ -317,6 +347,20 @@ elif codi== 103 and usuari=="Dwight Schrute" or codi==104 and usuari=="Jim Halpe
                     time.sleep(3)
                     break
             
+        elif opcio==4:
+            ventas_total=sum(paper_venutA2)+sum(paper_venutA3)+sum(paper_venutA4)+sum(paper_venutA5)+sum(paper_venutA6)            
+            print(f"La empresesa ha venut aquesta quantitat de paquets de A2: {paper_venutA2}")
+            print(f"La empresesa ha venut aquesta quantitat de paquets de A3: {paper_venutA3}")
+            print(f"La empresesa ha venut aquesta quantitat de paquets de A4: {paper_venutA4}")
+            print(f"La empresesa ha venut aquesta quantitat de paquets de A5: {paper_venutA5}")
+            print(f"La empresesa ha venut aquesta quantitat de paquets de A6: {paper_venutA6}")
+            print(f"i en total ha venut {ventas_total}")
+            break
+        elif opcio==5:
+            benefici_total=sum(volum_vendas)
+            print(f"La empresa ha guanyat {volum_vendas} en cada compra")
+            print(f"i en total ha guanyat {benefici_total}")
+            break
         elif opcio==6:
             print("Tancant programa ...")
             time.slepp(4)
